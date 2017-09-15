b7baeed14e0001cb000094c5b57aaddcaed9bc82
Zach Dobbs <zld6fd@mail.missouri.edu>
2017-09-14 20:43:53 -0500

    Resolve same comment

--- FlaskApp/FlaskApp.py
+++ FlaskApp/FlaskApp.py
@@ -75,7 +75,7 @@ def show_entries():
 @app.route('/add', methods=['POST'])
 def add_entry():
 	if not session.get('logged_in'):
-		#abort(401)
+		abort(401)
 	db = get_db()
 	db.execute('insert into entries (title, text) values (?, ?)',
 			[request.form['title'], request.form['text']])
